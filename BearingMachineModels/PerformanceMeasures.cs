c2d00a32e9dd2291eb6b1dfc6ef067aaed72ae20
HayaLababidi <38756711+HayaLababidi@users.noreply.github.com>
2018-11-27 15:40:10 +0200

    PerformanceMeasures calculations

--- BearingMachineModels/PerformanceMeasures.cs
+++ BearingMachineModels/PerformanceMeasures.cs
@@ -18,5 +18,49 @@ namespace BearingMachineModels
         public decimal DowntimeCost { get; set; }
         public decimal RepairPersonCost { get; set; }
         public decimal TotalCost { get; set; }
+
+        public void Current_PerformanceMeasures(List<CurrentSimulationCase> simulation_table, int DowntimeCost_in, int RepairPersonCost_in, int BearingCost_in, int RepairTimeForOneBearing)
+        {
+            //Cost of bearings = # bearings × Cost/bearing 
+            BearingCost = simulation_table.Count * BearingCost_in;
+
+            //Cost of delay time = total minutes × cost/minute = $1650
+            int total_minutes = 0;
+            for(int i=0;i<simulation_table.Count;i++)
+            {
+                total_minutes+=simulation_table[i].Delay;
+            }
+            DelayCost = total_minutes * DowntimeCost_in;
+
+            //Cost of downtime during repair = # bearings × RepairTimeForOneBearing × cost/minute
+            DowntimeCost = simulation_table.Count * RepairTimeForOneBearing * DowntimeCost_in;
+
+            //Cost of repairpersons = # bearings × RepairTimeForOneBearing × RepairPersonCost_in/minute
+            RepairPersonCost = simulation_table.Count * RepairTimeForOneBearing * RepairPersonCost_in/60;
+
+            TotalCost = BearingCost + DelayCost + DowntimeCost + RepairPersonCost;
+        }
+
+        public void Proposed_PerformanceMeasures(List<ProposedSimulationCase> simulation_table,int numOfBearing, int DowntimeCost_in, int RepairPersonCost_in, int BearingCost_in, int RepairTimeForAllBearing)
+        {
+            //Cost of bearings = # bearings × Cost/bearing 
+            BearingCost = simulation_table.Count * numOfBearing * BearingCost_in;
+
+            //Cost of delay time = total minutes × cost/minute = $1650
+            int total_minutes = 0;
+            for (int i = 0; i < simulation_table.Count; i++)
+            {
+                total_minutes += simulation_table[i].Delay;
+            }
+            DelayCost = total_minutes * DowntimeCost_in;
+
+            //Cost of downtime during repair = # bearings × RepairTimeForOneBearing × cost/minute
+            DowntimeCost = simulation_table.Count * numOfBearing * RepairTimeForAllBearing * DowntimeCost_in;
+
+            //Cost of repairpersons = # bearings × RepairTimeForOneBearing × RepairPersonCost_in/minute
+            RepairPersonCost = simulation_table.Count * numOfBearing * RepairTimeForAllBearing * RepairPersonCost_in / 60;
+
+            TotalCost = BearingCost + DelayCost + DowntimeCost + RepairPersonCost;
+        }
     }
 }
